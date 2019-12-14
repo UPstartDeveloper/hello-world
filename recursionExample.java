35994d747b5d9e5284766b5f1af3f23b01af0072
= <zainr7989@gmail.com>
2019-12-14 13:36:10 -0800

    [ADD] Methods to show recursion in action.

---
+++ recursionExample.java
@@ -0,0 +1,22 @@
+/* import 8 */
+
+public class keepAdding
+{
+    /*
+     * Demonstrates recursion using additive operations.
+     * Pre: int num < 2
+     * Post: int num = 2
+     */
+    public static int add(int num)
+    {
+        return 1;
+    }
+
+    /*
+     * Calls the recursive method add().
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
