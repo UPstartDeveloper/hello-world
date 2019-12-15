619f2d1d02b9b42ef3fa5ff7b41714c9aa84cfa3
= <zainr7989@gmail.com>
2019-12-14 18:01:45 -0800

    Implement add method.

--- recursionExample.java
+++ recursionExample.java
@@ -5,11 +5,18 @@ public class keepAdding
     /*
      * Demonstrates recursion using additive operations.
      * Pre: int num < 2
-     * Post: int num = 2
+     * Post: int num >= 2
      */
     public static int add(int num)
     {
-        return 1;
+        if(num < 2)
+        {
+            return add(num + 1);
+        }
+        else
+        {
+            return num;
+        }
     }
 
     /*
@@ -17,6 +24,9 @@ public class keepAdding
      */
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println(add(-4)); // prints 2
+        System.out.println(add(0)); // prints 2
+        System.out.println(add(2)); // prints 2
+        System.out.println(add(5)); // prints 5
     }
 }
